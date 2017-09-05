f770b75acd26ce03e89c4c33911854d1e3ac7096
Jonathan Kalbfeld <jonathan@thoughtwave.com>
2017-09-05 18:47:54 +0000

    build a couple more servers

--- helloworld.tf
+++ helloworld.tf
@@ -6,10 +6,14 @@ provider "openstack" {
   region      = "RegionOne"
 }
 
-# Create a web server
-resource "openstack_compute_instance_v2" "test-server" {
-  name = "test"
+resource "openstack_compute_instance_v2" "www" {
+  name = "www"
   image_id   = "e28ba5de-9b3b-4867-8fb4-3dbc8c9c79a9"
   flavor_name   = "c1.large"
 }
 
+resource "openstack_compute_instance_v2" "db" {
+  name = "db"
+  image_id   = "e28ba5de-9b3b-4867-8fb4-3dbc8c9c79a9"
+  flavor_name   = "c1.large"
+}
