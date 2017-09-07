18a34d56e0ae6fc4549f9776acddcde31e7aabc0
Jonathan Kalbfeld <jonathan@thoughtwave.com>
2017-09-07 17:49:36 +0000

    Adding security group access to Cassandra DB port

--- helloworld.tf
+++ helloworld.tf
@@ -37,6 +37,47 @@ resource "openstack_networking_secgroup_rule_v2" "admins_sg_rule_2" {
   security_group_id = "${openstack_networking_secgroup_v2.admins_sg.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "admins_sg_rule_2" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 9042
+  port_range_max    = 9042
+  remote_ip_prefix  = "199.188.96.62/32"
+  security_group_id = "${openstack_networking_secgroup_v2.admins_sg.id}"
+}
+
+
+resource "openstack_networking_secgroup_rule_v2" "admins_sg_rule_3" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 9042
+  port_range_max    = 9042
+  remote_ip_prefix  = "${openstack_networking_floatingip_v2.www_ip.address}/32"
+  security_group_id = "${openstack_networking_secgroup_v2.admins_sg.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "admins_sg_rule_4" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.admins_sg.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "admins_sg_rule_5" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.admins_sg.id}"
+}
+
 resource "openstack_networking_floatingip_v2" "www_ip" {
   pool = "admin_floating_net"
 }
