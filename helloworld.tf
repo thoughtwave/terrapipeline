59a6f0e15c10ccf570b891519fe3cb15fd34e959
Jonathan Kalbfeld <jonathan@thoughtwave.com>
2017-09-07 23:17:52 +0000

    Had a duplicate security group rule.

--- helloworld.tf
+++ helloworld.tf
@@ -37,17 +37,6 @@ resource "openstack_networking_secgroup_rule_v2" "admins_sg_rule_2" {
   security_group_id = "${openstack_networking_secgroup_v2.admins_sg.id}"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "admins_sg_rule_3" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 9042
-  port_range_max    = 9042
-  remote_ip_prefix  = "199.188.96.62/32"
-  security_group_id = "${openstack_networking_secgroup_v2.admins_sg.id}"
-}
-
-
 resource "openstack_networking_secgroup_rule_v2" "admins_sg_rule_4" {
   direction         = "ingress"
   ethertype         = "IPv4"
