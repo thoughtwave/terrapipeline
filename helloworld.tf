0fe596648e7daf0edfe41e704e6be920563c39f5
Jonathan Kalbfeld <jonathan@thoughtwave.com>
2017-09-07 07:30:49 +0000

    add floating IPs

--- helloworld.tf
+++ helloworld.tf
@@ -6,6 +6,14 @@ provider "openstack" {
   region      = "RegionOne"
 }
 
+resource "openstack_compute_floatingip_v2" "www_ip" {
+  pool = "admin_floating_net"
+}
+
+resource "openstack_compute_floatingip_v2" "db_ip" {
+  pool = "admin_floating_net"
+}
+
 resource "openstack_compute_instance_v2" "www" {
   name = "www"
   image_id   = "9f9d86b7-85f6-48a6-af04-b096dfecee11"
